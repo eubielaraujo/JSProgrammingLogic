cf5079d41763b32c3aee770d7cc0e624efb043de
Gabriel Araujo <gabrielaraujo2334@gmail.com>
2025-12-23 18:06:14 -0300

    Correção das condições do Switch

--- ClassificadorNivelHeroi/Classifier.js
+++ ClassificadorNivelHeroi/Classifier.js
@@ -1,5 +1,5 @@
 let NomeDoHeroi = "Ezio Auditore"
-let XPHeroi = 8345
+let XPHeroi = 8356
 let Nivel
 
 function classificarHeroi(Nivel) {
@@ -10,22 +10,22 @@ switch (true) {
     case XPHeroi < 1000:
         classificarHeroi("Ferro");
         break;
-    case XPHeroi >= 1001 && XPHeroi < 2000:
+    case XPHeroi >= 1001 && XPHeroi <= 2000:
         classificarHeroi("Bronze");
         break;
-    case XPHeroi >= 2001 && XPHeroi < 5000:
+    case XPHeroi >= 2001 && XPHeroi <= 5000:
         classificarHeroi("Prata");
         break;
-    case XPHeroi >= 5001 && XPHeroi < 7000:
+    case XPHeroi >= 5001 && XPHeroi <= 7000:
         classificarHeroi("Ouro");
         break;
-    case XPHeroi >= 7001 && XPHeroi < 8000:
+    case XPHeroi >= 7001 && XPHeroi <= 8000:
         classificarHeroi("Platina");
         break;
-    case XPHeroi >= 8001 && XPHeroi < 9000:
+    case XPHeroi >= 8001 && XPHeroi <= 9000:
         classificarHeroi("Ascendente");
         break;
-    case XPHeroi >= 9001 && XPHeroi < 10000:
+    case XPHeroi >= 9001 && XPHeroi <= 10000:
         classificarHeroi("Imortal");
         break;
     case XPHeroi >= 10001:
